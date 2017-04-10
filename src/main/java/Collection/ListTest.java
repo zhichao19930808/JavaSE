277ff07c6ff28b1e89ddb2cab0d96989754bf183
zhichao19930808 <zhichaoglory@163.com>
2017-04-10 20:05:20 +0800

    chapter 4、框架集合（总括）

---
+++ src/main/java/Collection/ListTest.java
@@ -0,0 +1,18 @@
+package Collection;
+
+/**
+ * Created by Administrator on 2017/4/10.
+ */
+public class ListTest {
+    public static void main(String[] args) {
+//        Iterable // 可迭代的,\ 迭代器 option + command + U / Ctrl + Alt + U
+
+//        Collection // 集合\ [kə'lekʃ\(ə\)n]
+
+//        List // 列表\ [lɪst]
+
+//        Set // 集\ [set]
+
+//        Map // 映射\ ['mæpɪŋ]
+    }
+}
