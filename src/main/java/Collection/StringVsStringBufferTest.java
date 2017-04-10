ed1d48d68e80af532892890235000a7e1a4b7707
zhichao19930808 <zhichaoglory@163.com>
2017-04-10 19:59:43 +0800

    字符串缓冲区（未整理）。

--- src/main/java/Collection/StringVsStringBufferTest.java
+++ src/main/java/Collection/StringVsStringBufferTest.java
@@ -10,8 +10,6 @@ public class StringVsStringBufferTest {
         String s = "";
         StringBuffer stringBuffer = new StringBuffer("");
         long start = System.currentTimeMillis();//currentTimeMillis   返回以毫秒为单位的当前时间。
-                                               // 注意，当返回值的时间单位是毫秒时，值的粒度取决于底层操作系统，并且粒度可能更大。
-                                              // 例如，许多操作系统以几十毫秒为单位测量时间。
         for (int i = 0; i < 10000; i++) {
 //            s += ALPHABET;
             stringBuffer.append(ALPHABET);
