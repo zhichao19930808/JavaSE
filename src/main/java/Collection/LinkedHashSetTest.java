32971037d9adf04581a3ffc3b57b0ddb6d1a95d1
zhichao19930808 <zhichaoglory@163.com>
2017-04-11 17:13:41 +0800

    LinkedHashSet

---
+++ src/main/java/Collection/LinkedHashSetTest.java
@@ -0,0 +1,25 @@
+package Collection;
+
+import java.util.LinkedHashSet;
+
+/**
+ * Created by Administrator on 2017/4/11.
+ */
+public class LinkedHashSetTest {
+    /*
+    * LinkedHashSet:
+    * 使用 HashMap 实现
+    * 按元素添加顺序排序*/
+    public static void main(String[] args) {
+    LinkedHashSet<String>strings=new LinkedHashSet<>();
+    strings.add("a");
+    strings.add("b");
+    strings.add("d");
+    strings.add("c");
+
+        System.out.println("size:"+strings.size());
+        for (String string : strings) {
+            System.out.println(string);
+        }
+    }
+}
