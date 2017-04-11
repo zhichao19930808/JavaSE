e8ac470d248d5396525ee0f1c524a5759fe6d6e6
zhichao19930808 <zhichaoglory@163.com>
2017-04-11 16:16:54 +0800

    hashSetTest

--- src/main/java/Collection/HashSetTest.java
+++ src/main/java/Collection/HashSetTest.java
@@ -7,6 +7,13 @@ import java.util.Set;
  * Created by Administrator on 2017/4/11.
  */
 public class HashSetTest {
+    /*
+    * 特点：
+    *       Set 不可以重复元素
+    *   1、使用 HashMap 存储元素
+    *   2、无序
+    *   3、效率高
+    * */
     public static void main(String[] args) {
         HashSet<String> strings = new HashSet<>();
         strings.add("hello");
