fe703e357db5055bbb954fe46fb1686c32f1a15e
zhichao19930808 <zhichaoglory@163.com>
2017-04-21 08:27:18 +0800

    基于字符的输入流

--- src/main/java/Io/ReaderTest.java
+++ src/main/java/Io/ReaderTest.java
@@ -6,14 +6,18 @@ import java.io.Reader;
 
 /**
  * Created by Administrator on 2017/4/18.
- * Reader 基于字符的输入流
+ * Reader 基于字符的输入流 reader ['riːdə(r)] n. 读者；读物；读本
  */
 public class ReaderTest {
+    //用于读取字符流的抽象类。子类必须实现的方法只有 read(char[], int, int) 和 close()。
+    // 但是，多数子类将重写此处定义的一些方法，以提供更高的效率和/或其他功能。
     public static void main(String[] args) {
         //JDK 7以上
-        try (Reader reader = new FileReader("test")) {
+        try (Reader reader = new FileReader("test")) {//FileReader 用来读取字符文件的便捷类。
+                                                                 // 此类的构造方法假定默认字符编码和默认字节缓冲区大小都是适当的。
+                                                                  // 要自己指定这些值，可以先在 FileInputStream 上构造一个 InputStreamReader。
             int i;
-            while ((i = reader.read()) != -1) {
+            while ((i = reader.read()) != -1) {//read 试图将字符读入指定的字符缓冲区。
                 System.out.print((char) i);
             }
         } catch (java.io.IOException e) {
