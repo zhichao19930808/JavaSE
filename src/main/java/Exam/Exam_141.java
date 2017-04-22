c3bac026dc229eefd807031e68a6cad5c4ff1321
zhichao19930808 <zhichaoglory@163.com>
2017-04-22 11:58:49 +0800

    编写一个没有处理的发生数组下标越界异常的程序

---
+++ src/main/java/Exam/Exam_141.java
@@ -0,0 +1,11 @@
+package Exam;
+
+/**
+ * Created by Administrator on 2017/4/22.
+ * 编写一个没有处理的发生数组下标越界异常的程序
+ */
+public class Exam_141 {
+    public static void main(String[] args) {
+        System.out.println(new String[]{"2","3","aa"}[4]);
+    }
+}
