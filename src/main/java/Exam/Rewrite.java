6f5391f58b91710ada06ed63f4a3f9ea32b02727
zhichao19930808 <zhichaoglory@163.com>
2017-05-06 14:51:52 +0800

    编写一个实现方法重载的程序

---
+++ src/main/java/Exam/Rewrite.java
@@ -0,0 +1,14 @@
+package Exam;
+
+/**
+ * Created by Administrator on 2017/5/6.
+ * 编写一个实现方法重载的程序
+ */
+public class Rewrite {
+    public void rewrit() {}
+    public void rewrit(double x) {}
+    public void rewrit(String x) {}
+    public void rewrit(String x,String y) {}
+    public void rewrit(String x,double y) {}
+    public void rewrit(double y,String x) {}
+}
