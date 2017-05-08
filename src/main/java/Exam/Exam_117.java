202869ac814ae97b632c29ab90add5e9f7ccd24f
zhichao19930808 <zhichaoglory@163.com>
2017-05-08 08:19:18 +0800

    找出字符串中出现次数最多的字母和出现的次数

--- src/main/java/Exam/Exam_117.java
+++ src/main/java/Exam/Exam_117.java
@@ -1,13 +1,37 @@
 package Exam;
 
+import java.util.Collections;
+import java.util.Hashtable;
+import java.util.Map;
+
 /**
  * Created by Administrator on 2017/4/15.
+ * 找出字符串中出现次数最多的字母和出现的次数
  */
 public class Exam_117 {
     /*
     找出字符串中出现次数最多的字母和出现的次数
      */
     public static void main(String[] args) {
+        String s="efweuhfleeeeewehflwklnlqkn";
+
+        Hashtable<Character, Integer> hashtable = new Hashtable<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            hashtable.put(c, hashtable.get(c) == null ? 1 : hashtable.get(c) + 1);
+        }
+
+        int max = Collections.max(hashtable.values());
+
+        System.out.println(max);
+
+        for (Map.Entry<Character, Integer> entry : hashtable.entrySet()) {
+            if (max == entry.getValue()) {
+                System.out.println(entry.getKey());
+            }
+        }
+        /*
         int a=0;
         int x=0;
         String s="efweuhfleeeeewehflwklnlqkn";
@@ -23,6 +47,6 @@ public class Exam_117 {
                 a=i;
             }
             }
-            System.out.println(s.charAt(a)+"出现次数最多"+"出现了"+x+"次");
+            System.out.println(s.charAt(a)+"出现次数最多"+"出现了"+x+"次");*/
     }
 }
