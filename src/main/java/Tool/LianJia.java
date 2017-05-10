b200cb717ef4f3dc45c9dff329f96fa2a2fc3930
zhichao19930808 <zhichaoglory@163.com>
2017-05-10 10:41:00 +0800

    抓取链家网东城地区房价的行情

--- src/main/java/Tool/LianJia.java
+++ src/main/java/Tool/LianJia.java
@@ -48,15 +48,15 @@ public class LianJia {
             Elements elements = document.select("li[class=clear]");
 //        System.out.println(elements);
             for (Element element : elements) {
-//4.利用迭代的方法取出每个li标签中"a[data-el=region]"标签中第一个元素的文本内容，即小区名称；
+//4.利用迭代的方法取出每个li标签中第一个"a[data-el=region]"标签中的文本内容，即小区名称；
                 String region = element.select("a[data-el=region]").first().text();//region ['riːdʒən]地区
-//5.利用迭代的方法取出每个li标签中"div[class=totalPrice]"标签中第一个元素的文本内容，即总价
+//5.利用迭代的方法取出每个li标签中第一个"div[class=totalPrice]"标签中的文本内容，即总价
                 String totalPrice = element.select("div[class=totalPrice]").first().text();
-//6.利用迭代的方法去除每个li标签中""div[class=houseInfo]"标签中第一个元素的内容
+//6.利用迭代的方法去除每个li标签中第一个""div[class=houseInfo]"标签中的内容
                 Element houseInfoElement = element.select("div[class=houseInfo]").first();
 //6.1获取 "div[class=houseInfo]"标签中第三个子节点的内容,即是房屋的详细信息
                 String houseInfo = houseInfoElement.childNode(2).toString();//childNodes属性:返回节点的子节点集合
-//7.利用迭代的方法取出每个li标签中"div[class=unitPrice]"标签中的第一个元素，使用attr方法取得他对应域"data-price"属性的一个值
+//7.利用迭代的方法取出每个li标签中第一个"div[class=unitPrice]"标签，使用attr方法取得他对应域"data-price"属性的一个值
                 String unitPrice = element.select("div[class=unitPrice]").first().attr("data-price");
 //                System.out.println("小区：" + region + "\n详情：" + houseInfo + "\t单价:" + unitPrice + "\t总价：" + totalPrice);
                 writer.write(region+"#"+houseInfo+"#"+unitPrice+"#"+totalPrice+"\n");
