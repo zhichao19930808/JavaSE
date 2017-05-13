5e188e4cdbe2b4820410e52e27a4e29fd54d12c4
zhichao19930808 <zhichaoglory@163.com>
2017-05-13 08:43:44 +0800

    使用 Runnable 接口实现多线程

--- src/main/java/Multithreading/Exam/Exam_2.java
+++ src/main/java/Multithreading/Exam/Exam_2.java
@@ -2,9 +2,9 @@ package Multithreading.Exam;
 
 /**
  * Created by Administrator on 2017/5/13.
- * 1. 使用 Thread 类实现多线程 10分
+ * 2.  使用 Runnable 接口实现多线程 10分
  */
-public class Exam_1 implements Runnable{
+public class Exam_2 implements Runnable{
     @Override
     public void run() {
         System.out.println("------------");
@@ -14,12 +14,12 @@ public class Exam_1 implements Runnable{
     }
 
     public static void main(String[] args) {
-        Thread thread = new Thread(new Exam_1());
-        Thread thread1 = new Thread(new Exam_1());
-        Thread thread2 = new Thread(new Exam_1());
+        Thread thread = new Thread(new Exam_2());
+        Thread thread1 = new Thread(new Exam_2());
+        Thread thread2 = new Thread(new Exam_2());
         thread.start();
         thread1.start();
         thread2.start();
-        System.out.println("Exam_1...");
+        System.out.println("Exam_2...");
     }
 }
