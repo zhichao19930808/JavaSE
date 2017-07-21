d0be86ecc225115cfe72ca097f2d2cb9ce1aacf0
zhichao19930808 <zhichaoglory@163.com>
2017-07-21 10:20:01 +0800

    服务器和客户端的简单连接

---
+++ src/main/java/serverAndclient/Test_1/Client.java
@@ -0,0 +1,41 @@
+package serverAndclient.Test_1;
+
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class Client {
+    //要连接的服务端的端口是多少
+    private static final int PORT = 1025;
+    //IP
+    //本地ip：localhost或127.0.0.1
+    private static final String IP_ADDRESS="localhost";
+
+    public void connctionServer() {
+
+        try {
+            //连接服务器
+            System.out.println("客户端上线，连接服务器");
+            Socket client = new Socket(IP_ADDRESS, PORT);
+            System.out.println("连接服务器成功");
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("输入你想对服务器说的话：");
+            String data = scanner.nextLine();
+            DataOutputStream outputStream = new DataOutputStream(client.getOutputStream());
+            outputStream.writeUTF(data);
+            outputStream.close();
+            client.close();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        Client demo = new Client();
+        demo.connctionServer();
+    }
+
+}
---
+++ src/main/java/serverAndclient/Test_1/Sever.java
@@ -0,0 +1,85 @@
+package serverAndclient.Test_1;
+
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/**
+ * 客户端连接成功后，和服务端say hello
+ * 服务器收到客户端的say hello以后，会给客户一个say hello
+ *
+ *
+ */
+public class Sever {
+    //指定要使用的端口
+    public static final int PORT = 1025;
+    //初始化
+    public void inInti() {
+        //告诉serSocket，客户端用上面端口号连接
+        try {
+            ServerSocket serverSocket = new ServerSocket(PORT);
+            //轮询
+            //等待客户端连接
+            System.out.println("服务器启动，等待客户端连接");
+            while(true) {
+                //accept()用于返回连接成功的客户端对象
+                Socket client =serverSocket.accept();
+                //当客户端成功连接之后，为每一个客户端开辟一个新的线程
+                new SocketThread(client);
+            }
+
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+    //内部类，最好是私有
+//	一旦当客户链接以后，为每一个客户端的连接对象开辟一个虚拟的子线程
+//	1，方便统计当前客户端的数量
+//	2，让服务器不会被卡死
+    private class SocketThread implements Runnable{
+
+        //用客户端的连接对象来操作连接的客户端
+        private Socket client = null;
+
+
+        public SocketThread(Socket client) {
+            //判断client是否是上一个客户端的客户端遗留的对象
+            //
+            if (this.client==null) {
+                this.client=client;
+            }
+            new Thread(this).start();//可以在任意地方创建线程
+        }
+        @Override
+        public void run() {
+            // TODO Auto-generated method stub
+            //处理消息
+            //获取客户端床底给服务器的数据
+            DataInputStream inputStream;
+            try {
+                inputStream = new DataInputStream(client.getInputStream());
+                //服务端的read方法要和客户端的write方法保持一致
+                String data = inputStream.readUTF();
+                System.out.println("从客户端传送过来的信息：\n"+data);
+
+
+                inputStream.close();
+                client.close();
+
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+
+        }
+
+    }
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        Sever demo = new Sever();
+        demo.inInti();
+    }
+
+}
